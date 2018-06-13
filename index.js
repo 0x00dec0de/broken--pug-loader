230c1e3b22944feea2743cf581fb81c520cec6f4
Brian M Hunt <bmh@tko.io>
2018-06-13 15:36:33 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+require('./one.pug')
+require('./two.pug')
+require('./abc.scss')
+require('./def.scss')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+//
+// Read the .yaml config file.
+//
+const path = require('path')
+
+module.exports = {
+  output: { filename: 'app.js' },
+  entry: path.join(__dirname, 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          { loader: 'file-loader', options: { name: 'index.html' } },
+          'extract-loader',
+          'html-loader',
+          'pug-html-loader'
+        ] },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'file-loader', options: { name: 'main.css' } },
+          'extract-loader',
+          'css-loader',
+          // { loader: 'postcss-loader', options: { plugins: () => [autoprefixer({ grid: false })] } },
+          { loader: 'sass-loader', options: { includePaths: ['./node_modules'] } },
+        ] }
+    ]
+  },
+}
